d3eb0dae5450798eb2b82473d9ac247d4137295b
Filirom1 <filirom1@gmail.com>
2014-10-01 23:15:12 +0200

    add puppet-bootstrap and my dotfiles

---
+++ README.md
@@ -0,0 +1,12 @@
+# Boot my dotfiles
+
+```
+distro=centos_7_x
+
+curl "https://raw.githubusercontent.com/Filirom1/puppet-bootstrap/master/$distro.sh" | sh
+puppet module install puppetlabs/vcsrepo
+puppet module install puppetlabs/nodejs
+
+curl -L "https://github.com/Filirom1/boot-my-dotfiles/archive/master.tar.gz" | tar zx
+puppet apply boot-my-dotfiles-master/manifests/init.pp 
+```
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,12 @@
 node default {
   include nodejs
+  $user='romain'
 
+  # standard packages
+  package { "strace": ensure => "latest" }
+  package { "sudo":   ensure => "latest" }
+
+  # os specific packages
   case $::osfamily {
      'Archlinux': {
        package { "gvim": ensure => "latest" }
@@ -16,6 +22,34 @@ node default {
      }
   }
 
-  package { "strace": ensure => "latest" }
-  package { "sudo":   ensure => "latest" }
+  # ensure user is present
+  user { "${user}":
+    ensure => present
+  }
+
+  # ensure dotfiles are present
+  $home_dir="/home/${user}"
+  vcsrepo { "${home_dir}/":
+    ensure   => present,
+    provider => git,
+    source => 'https://github.com/Filirom1/dotfiles.vim',
+  }
+
+  #ensure vundle is installed
+  $vim_dir = "${home_dir}/.vim"
+  $bundle_dir = "${vim_dir}/bundle"
+  $vundle_dir = "${bundle_dir}/Vundle.vim"
+  file { [$vim_dir, $bundle_dir]:
+    ensure => 'directory',
+  } ->
+  vcsrepo { "$vundle_dir":
+    ensure   => present,
+    provider => git,
+    source => 'https://github.com/gmarik/Vundle.vim',
+  } ->
+  exec { 'BundleInstall':
+    command => "sudo ${romain} vim +PluginInstall +qall",
+    refreshonly => true,
+    path => $::path,
+  }
 }
